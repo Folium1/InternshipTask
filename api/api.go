b3692a5b8a571af83ee73b4b74aafe05ee6ae498
Folium1 <vampirowitch.vampir@gmail.com>
2023-06-02 12:21:18 +0300

    Refactoring

--- api/api.go
+++ api/api.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"strconv"
 
-	dto "book/DTO"
 	"book/config"
+	"book/models"
 	"book/service"
 
 	"github.com/gin-gonic/gin"
@@ -18,86 +18,98 @@ var (
 
 func StartServer() {
 	r := gin.Default()
-
-	r.GET("/book/:id", func(ctx *gin.Context) {
-		bookId, ok := ctx.Params.Get("id")
-		if !ok {
-			config.Logger.Error(fmt.Sprintf("Couldn't get id parameter from context path:%v method:%v", ctx.Request.URL.Path, ctx.Request.Method))
-			ctx.JSON(http.StatusBadRequest, nil)
-			return
-		}
-		book, err := bookService.Get(bookId)
-		if err != nil {
-			config.Logger.Error(fmt.Sprintf("Coudn't get book by it's id,err: %v", err))
-			ctx.JSON(http.StatusNotFound, "Not Found")
-			return
-		}
-		ctx.JSON(200, book)
-	})
-	r.GET("/books", func(ctx *gin.Context) {
-		books, err := bookService.GetAll()
-		if err != nil {
-			config.Logger.Error(fmt.Sprintf("Couldn't get all books,err: %v", err))
-			ctx.JSON(http.StatusInternalServerError, err)
-			return
-		}
-		ctx.JSON(http.StatusOK, books)
-	})
-	r.POST("/create/", func(ctx *gin.Context) {
-		var book dto.BookDTO
-		err := ctx.ShouldBindJSON(&book)
-		if err != nil {
-			config.Logger.Error(fmt.Sprintf("Couldn't parse book data from request,err: %v", err))
-			ctx.JSON(http.StatusBadRequest, nil)
-			return
-		}
-		id, err := bookService.Create(book)
-		if err != nil {
-			config.Logger.Error(fmt.Sprintf("Couldn't create new book instace, err: %v", err))
-			ctx.JSON(http.StatusInternalServerError, err)
-			return
-		}
-		ctx.JSON(200, id)
-	})
-	r.PUT("/update/:id", func(ctx *gin.Context) {
-		id, ok := ctx.Params.Get("id")
-		if !ok {
-			config.Logger.Error(fmt.Sprintf("Couldn't get id parameter from context path:%v method:%v", ctx.Request.URL.Path, ctx.Request.Method))
-			ctx.JSON(http.StatusBadRequest, nil)
-			return
-		}
-		intId, err := strconv.Atoi(id)
-		if err != nil {
-			config.Logger.Error(fmt.Sprintf("Couldn't convert id from string to int,err: %v", err))
-			ctx.JSON(http.StatusInternalServerError, err)
-			return
-		}
-		newData := dto.BookDTO{}
-		newData.ID = intId
-
-		err = ctx.ShouldBindJSON(&newData)
-		if err != nil {
-			config.Logger.Error(fmt.Sprintf("Couldn't parse updated book data from request,err: %v", err))
-			ctx.JSON(http.StatusInternalServerError, err)
-			return
-		}
-		bookService.Update(newData)
-	})
-	r.DELETE("/delete/:id", func(ctx *gin.Context) {
-		id, ok := ctx.Params.Get("id")
-		if !ok {
-			config.Logger.Error(fmt.Sprintf("Couldn't get id parameter from context path:%v method:%v", ctx.Request.URL.Path, ctx.Request.Method))
-			ctx.JSON(http.StatusBadRequest, nil)
-			return
-		}
-		err := bookService.Delete(id)
-		if err != nil {
-			config.Logger.Error(fmt.Sprintf("Couldn't delete book from db,err:%v", err))
-			ctx.JSON(http.StatusInternalServerError, err)
-			return
-		}
-		ctx.JSON(200, nil)
-	})
-
+	books := r.Group("/")
+	{
+		books.GET("/book/:id", Book)
+		books.GET("/books", Books)
+		books.POST("/create/", Create)
+		books.PUT("/update/:id", Update)
+		books.DELETE("/delete/:id", Delete)
+	}
 	r.Run(":9090")
+}
+
+func Book(ctx *gin.Context) {
+	bookId, ok := ctx.Params.Get("id")
+	if !ok {
+		config.Logger.Error(fmt.Sprintf("Couldn't get id parameter from context path:%v method:%v", ctx.Request.URL.Path, ctx.Request.Method))
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	book, err := bookService.Get(bookId)
+	if err != nil {
+		config.Logger.Error(fmt.Sprintf("Coudn't get book by it's id,err: %v", err))
+		ctx.JSON(http.StatusNotFound, "Not Found")
+		return
+	}
+	ctx.JSON(200, book)
+}
+
+func Books(ctx *gin.Context) {
+	books, err := bookService.GetAll()
+	if err != nil {
+		config.Logger.Error(fmt.Sprintf("Couldn't get all books,err: %v", err))
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, books)
+}
+
+func Create(ctx *gin.Context) {
+	var book models.Book
+	err := ctx.ShouldBindJSON(&book)
+	if err != nil {
+		config.Logger.Error(fmt.Sprintf("Couldn't parse book data from request,err: %v", err))
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	id, err := bookService.Create(book)
+	if err != nil {
+		config.Logger.Error(fmt.Sprintf("Couldn't create new book instace, err: %v", err))
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	ctx.JSON(200, id)
+}
+
+func Update(ctx *gin.Context) {
+	id, ok := ctx.Params.Get("id")
+	if !ok {
+		config.Logger.Error(fmt.Sprintf("Couldn't get id parameter from context path:%v method:%v", ctx.Request.URL.Path, ctx.Request.Method))
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		config.Logger.Error(fmt.Sprintf("Couldn't convert id from string to int,err: %v", err))
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	newData := models.Book{}
+	newData.ID = intId
+
+	err = ctx.ShouldBindJSON(&newData)
+	if err != nil {
+		config.Logger.Error(fmt.Sprintf("Couldn't parse updated book data from request,err: %v", err))
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	bookService.Update(newData)
+	ctx.JSON(200, nil)
+}
+
+func Delete(ctx *gin.Context) {
+	id, ok := ctx.Params.Get("id")
+	if !ok {
+		config.Logger.Error(fmt.Sprintf("Couldn't get id parameter from context path:%v method:%v", ctx.Request.URL.Path, ctx.Request.Method))
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	err := bookService.Delete(id)
+	if err != nil {
+		config.Logger.Error(fmt.Sprintf("Couldn't delete book from db,err:%v", err))
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	ctx.JSON(200, nil)
 }
